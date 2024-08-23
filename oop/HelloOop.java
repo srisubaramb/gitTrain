b2f0ba3c295de46d82f75a0a5bfecba18683b9ea
srisubaramb <161054356+srisubaramb@users.noreply.github.com>
2024-08-23 04:27:34 +0000

    HelloOop ceated

---
+++ oop/HelloOop.java
@@ -0,0 +1,11 @@
+class Demo {
+    public void sayHello(){
+        System.out.println("HelloOop");
+    }
+}
+class HelloOop {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.sayHello();
+    }
+}
